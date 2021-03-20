c574717aff4d1d2684c48ec2a82a2de4aeb378c0
Anshuman Gupta <anshuman844@gmail.com>
2021-03-20 22:38:35 +0530

    feat: Created Footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,30 @@
+import styles from '../../styles/components/footer.module.scss';
+import { developer } from '../../finacular';
+
+const Footer = props => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__social}>
+        {
+          developer.social.map(({link, title, icon}, idx) => (
+            <a href={link} target="_blank" rel="noreferrer" key={idx}>
+              <i className={icon}></i>
+              <span>{title}</span>
+            </a>
+          ))
+        }
+      </div>
+      <p className={styles.footer__dev}>
+        Developed with ❤️️ by&nbsp;
+        <a href={developer.url} target="_blank" rel="noreferrer">
+          {developer.name}
+        </a>
+        <p className={styles.footer__copyright}>
+          &copy; 2021 {developer.name}
+        </p>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
