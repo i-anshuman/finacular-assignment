383d494aa774103c64023d48c7706f6b4d22006e
Anshuman Gupta <anshuman844@gmail.com>
2021-03-22 09:29:29 +0530

    feat: Add dark theme

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,11 +3,17 @@ import { title } from '../../finacular';
 import styles from '../../styles/components/header.module.scss';
 
 const Header = props => {
+  const changeTheme = () => {
+    const body = document.querySelector('body')
+    const theme = body.getAttribute('data-theme');
+    body.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
     <header className={styles.header}>
       <div className={ styles.header__inner }>
         <p className={ styles.brand }>{title}</p>
-        <Button className={ styles.theme_toggle }>
+        <Button className={ styles.theme_toggle } onClick={changeTheme}>
           <i className="fa fa-sun"></i>
         </Button>
       </div>
